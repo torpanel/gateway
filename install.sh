6276a89b208d64e6222348a4ed916efd37840606
torpanel <torpanel@sigaint.org>
2017-01-11 04:04:34 -0800

    Fixed spelling error.

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # Packages needed to run gateway software
 apt-get install php5-cli php5-json
 
-cp torrc /etc/torrc
+cp torrc /etc/tor/torrc
 cp torpanel-gateway /usr/local/bin/torpanel-gateway
 cp torpanel-gateway.php /usr/local/bin/torpanel-gateway.php
+
+service tor reload
