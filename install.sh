b2d3f39c51bd58c85467a4a2c2dfafbaeced238b
torpanel <torpanel@sigaint.org>
2017-01-11 04:00:54 -0800

    Fixed permissions. Renamed script.

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 # Packages needed to run gateway software
 apt-get install php5-cli php5-json
 
-cp torpanel-gateway.sh /usr/local/bin/torpanel-gateway.sh
+cp torrc /etc/torrc
+cp torpanel-gateway /usr/local/bin/torpanel-gateway
 cp torpanel-gateway.php /usr/local/bin/torpanel-gateway.php
